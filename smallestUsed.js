8405f23516593857cec7ad02bc24e25f8b92f595
mmorgan86 <michael1986morgan@gmail.com>
2020-08-18 23:11:56 -0400

    added smallused.js

---
+++ smallestUsed.js
@@ -0,0 +1,29 @@
+/*
+Hey awesome programmer!
+
+You've got much data to manage and of course you use zero-based and non-negative ID's to make each data item unique!
+
+Therefore you need a method, which returns the smallest unused ID for your next new data item...
+
+Note: The given array of used IDs may be unsorted. For test reasons there may be duplicate IDs, but you don't have to find or remove them!
+
+Go on and code some pure awesomeness!
+
+Test.assertEquals(nextId([0,1,2,3,5]), 4);
+Test.assertEquals(nextId([0,1,2,3,4,5,6,7,8,9,10]), 11);
+*/
+
+function nextId(ids) {
+    let sortedIds = ids.sort((a, b) => a - b);
+    for(let i = 0; i < ids.length; i++) {
+
+        if(!ids.includes(i)) {
+            return i;
+        }
+    }
+    return ids.length;
+}
+
+// let ids = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; // return 11
+let ids = [0, 1, 2, 3, 5, 2 ,3]; // return 4
+console.log(nextId(ids));
