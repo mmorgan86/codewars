1146a2ba7de50634f49744ce27503af07e8aa8ff
mmorgan86 <michael1986morgan@gmail.com>
2020-08-18 21:30:15 -0400

    removed .history directory

--- .history/funList_20200817150936.js
+++
@@ -1,8 +0,0 @@
-function indexOf(head, value) {
-  return head.indexOf(value) !== -1;
-}
-
-let head = [1, 2, 3];
-let value = 2;
-
-console.log(indexOf(head, value))
--- .history/funList_20200817151120.js
+++
@@ -1,8 +0,0 @@
-function indexOf(head, value) {
-  return head.map((h, i) => h === value ? i : -1);
-}
-
-let head = [1, 2, 3];
-let value = 2;
-
-console.log(indexOf(head, value))
